d519fca2af1f69f09bc22ead94261415500b45c1
Cem Aydin <cem.aydin@gmx.ch>
2022-11-11 15:24:41 +0100

    fix create/edit/delete entry (server side, post/put/delete)

--- web/src/routes/entry/[entryId]/+server.ts
+++ web/src/routes/entry/[entryId]/+server.ts
@@ -1,17 +1,17 @@
 import { json, error } from '@sveltejs/kit';
 import { allowedTypes } from '$lib/entryTypes.js';
 
-export async function GET(request) {
+export async function GET({ request, params, locals }) {
 
-  const db = request.locals.db;
+  const db = locals.db;
 
-  const entryId = request.params.entryId;
+  const entryId = params.entryId;
   const user = 'rebootl';
 
   const c = await db.collection('entries');
 
   const q = { user: user, id: entryId, deleted: false, last: true };
-  if (!request.locals.loggedIn || request.locals?.user !== user) {
+  if (!locals.loggedIn || locals?.user !== user) {
     q.private = false;
   }
 
@@ -25,56 +25,52 @@ export async function GET(request) {
 const requiredFields = [ 'id', 'date', 'user', 'type', 'topics', 'tags',
   'private', 'pinned', 'last', 'version' ];
 
-export async function POST(request) {
+export async function POST({ request, locals }) {
   //console.log(request)
 
-  const b = request.body;
+  const b = await request.json();
 
   // user logged in and username in entry
-  if (!request.locals.loggedIn || request.locals?.user !== b.user) {
-    return { status: 403 }
+  if (!locals.loggedIn || locals?.user !== b.user) {
+    throw error(403);
   }
 
   // checks
   for (const f of requiredFields) {
-    if (!b.hasOwnProperty(f)) return { status: 400 };
+    if (!b.hasOwnProperty(f)) throw error(400);
   }
 
-  if (!allowedTypes.includes(b.type)) return { status: 400 };
+  if (!allowedTypes.includes(b.type)) throw error(400);
 
-  const db = request.locals.db;
+  const db = locals.db;
   const c = await db.collection('entries');
   const r = await c.insertOne(b);
-  if (!r) return { status: 400 };
+  if (!r) throw error(400);
 
-  return {
-    body: {
-      result: r
-    }
-  };
+  return json({ result: r });
 }
 
-export async function PUT(request) {
+export async function PUT({ request, locals }) {
   //console.log(request)
 
-  const b = request.body;
+  const b = await request.json();
 
   // user logged in and username in entry
-  if (!request.locals.loggedIn || request.locals?.user !== b.user) {
-    return { status: 403 }
+  if (!locals.loggedIn || locals?.user !== b.user) {
+    throw error(403);
   }
 
   // checks
   for (const f of requiredFields) {
-    if (!b.hasOwnProperty(f)) return { status: 400 };
+    if (!b.hasOwnProperty(f)) throw error(400);
   }
 
-  if (!allowedTypes.includes(b.type)) return { status: 400 };
+  if (!allowedTypes.includes(b.type)) throw error(400);
 
   // immutable field has to be deleted
   delete b._id;
 
-  const db = request.locals.db;
+  const db = locals.db;
   const c = await db.collection('entries');
   const r = await c.updateOne(
     { user: b.user, id: b.id, version: b.version },
@@ -82,42 +78,34 @@ export async function PUT(request) {
       last: false,
     }
   });
-  if (!r) return { status: 400 };
+  if (!r) throw error(400);
 
   b.version++;
 
   const rn = await c.insertOne(b);
-  if (!rn) return { status: 400 };
+  if (!rn) throw error(400);
 
-  return {
-    body: {
-      result: rn
-    }
-  };
+  return json({ result: r });
 }
 
-export async function DEL(request) {
+export async function DELETE({ request, locals }) {
   //console.log(request)
 
-  const b = request.body;
+  const b = await request.json();
 
   // user logged in and username in entry
-  if (!request.locals.loggedIn || request.locals?.user !== b.user) {
-    return { status: 403 }
+  if (!locals.loggedIn || locals?.user !== b.user) {
+    throw error(403);
   }
 
-  const db = request.locals.db;
+  const db = locals.db;
   const c = await db.collection('entries');
   //const r = await c.deleteOne({ id: b.id });
   const r = await c.updateMany({ user: b.user, id: b.id }, { $set: {
     deleted: true,
     deleteDate: new Date()
   }});
-  if (!r?.modifiedCount) return { status: 400 };
+  if (!r?.modifiedCount) throw error(400);
 
-  return {
-    body: {
-      result: r
-    }
-  };
+  return json({ result: r });
 }
