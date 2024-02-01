a9df7aa66c3febb4c1316f8735a592f44a90252c
Cem Aydin <cem.aydin@gmx.ch>
2024-02-01 21:20:09 +0100

    pre-process timeline entries on server side instead of client

--- s5/src/routes/timeline/+page.server.js
+++ s5/src/routes/timeline/+page.server.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs';
+
 import { error } from '@sveltejs/kit';
 import { getEntries } from '$lib/server/db.js';
 
@@ -12,8 +14,67 @@ export async function load({ locals }) {
     loggedIn = true;
   }
 
-  const r = getEntries(userId, type, loggedIn);
-  if (!r) throw error(404, 'Not found');
+  const entries = getEntries(userId, type, loggedIn);
+  if (!entries) throw error(404, 'Not found');
 
-  return { entries: r };
+  let previousYear = dayjs(entries[0].created_at).format('YYYY');
+  let previousMonth = dayjs(entries[0].created_at).format('MMMM');
+
+  // we want to display a timeline with entries grouped by year and month
+  // we do this by creating a new array with entries and year/month headers
+  // we also sort the entries by date
+
+  /** @typedef {Object} TimelineEntry
+    * @property {'year' | 'month' | 'entry'} type
+    * @property {string | null} year
+    * @property {string | null} month
+    * @property {import('$lib/server/db.js').EntryData | null} entry
+    * @property {string | null} date
+    */
+  /** @type {TimelineEntry[]} */
+  const timelineEntries = [];
+
+  const sortedEntries = entries.sort((a, b) => {
+    const aDate = a.manual_date || a.created_at;
+    const bDate = b.manual_date || b.created_at;
+    return dayjs(aDate).isBefore(dayjs(bDate)) ? 1 : -1;
+  });
+
+  for (const entry of sortedEntries) {
+    const entryDate = entry.manual_date || entry.created_at;
+
+    const entryYear = dayjs(entryDate).format('YYYY');
+    const entryMonth = dayjs(entryDate).format('MMMM');
+    // console.log(entryYear);
+    // console.log(entryMonth);
+    if (entryYear !== previousYear) {
+      timelineEntries.push({
+        type: 'year',
+        year: entryYear,
+        month: null,
+        entry: null,
+        date: null,
+      });
+      previousYear = entryYear;
+    }
+    if (entryMonth !== previousMonth) {
+      timelineEntries.push({
+        type: 'month',
+        year: null,
+        month: entryMonth,
+        entry: null,
+        date: null,
+      });
+      previousMonth = entryMonth;
+    }
+    timelineEntries.push({
+      type: 'entry',
+      year: null,
+      month: null,
+      entry: entry,
+      date: entryDate,
+    });
+  }
+
+  return { timelineEntries };
 }
--- s5/src/routes/timeline/+page.svelte
+++ s5/src/routes/timeline/+page.svelte
@@ -1,91 +1,7 @@
 <script>
-  import dayjs from 'dayjs';
 
-  import { onMount } from 'svelte';
-
-	// export let data;
   let { data } = $props();
   // console.log(data);
-  
-	let entries = data.entries;
-  // console.log(entries);
-
-  let previousYear = dayjs(entries[0].created_at).format('YYYY');
-  let previousMonth = dayjs(entries[0].created_at).format('MMMM');
-  
-  // we want to display a timeline with entries grouped by year and month
-  // we do this by creating a new array with entries and year/month headers
-  // we also sort the entries by date
-  
-  /** @typedef {Object} TimelineEntry
-    * @property {'year' | 'month' | 'entry'} type
-    * @property {string | null} year
-    * @property {string | null} month
-    * @property {import('$lib/server/db.js').EntryData | null} entry
-    * @property {string | null} date
-    */
-  /** @type {TimelineEntry[]} */
-  let timelineEntries = $state([
-    {
-      type: 'year',
-      year: previousYear,
-      month: null,
-      entry: null,
-      date: null,
-    },
-    {
-      type: 'month',
-      year: null,
-      month: previousMonth,
-      entry: null,
-      date: null,
-    },
-  ]);
-
-  onMount(() => {
-    const sortedEntries = entries.sort((a, b) => {
-      const aDate = a.manual_date || a.created_at;
-      const bDate = b.manual_date || b.created_at;
-      return dayjs(aDate).isBefore(dayjs(bDate)) ? 1 : -1;
-    });
-
-    for (const entry of sortedEntries) {
-      const entryDate = entry.manual_date || entry.created_at;
-  
-      const entryYear = dayjs(entryDate).format('YYYY');
-      const entryMonth = dayjs(entryDate).format('MMMM');
-      // console.log(entryYear);
-      // console.log(entryMonth);
-      if (entryYear !== previousYear) {
-        timelineEntries.push({
-          type: 'year',
-          year: entryYear,
-          month: null,
-          entry: null,
-          date: null,
-        });
-        previousYear = entryYear;
-      }
-      if (entryMonth !== previousMonth) {
-        timelineEntries.push({
-          type: 'month',
-          year: null,
-          month: entryMonth,
-          entry: null,
-          date: null,
-        });
-        previousMonth = entryMonth;
-      }
-      timelineEntries.push({
-        type: 'entry',
-        year: null,
-        month: null,
-        entry: entry,
-        date: entryDate,
-      });
-    }
-    // console.log(timelineEntries);
-  });
 
 </script>
 
@@ -96,7 +12,7 @@
 {/if}
 
 <div class="list">
-  {#each timelineEntries as t}
+  {#each data.timelineEntries as t}
     {#if t.type === 'year'}
       <h2 class="year">{t.year}</h2>
     {:else if t.type === 'month'}
